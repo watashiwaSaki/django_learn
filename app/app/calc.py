63bdd37a23297d523f7758563a6f370a4daecdd9
chauhan_ <watashiwa.sakshi@gmail.com>
2024-09-16 17:12:48 +0200

    Added the requirements to run Postgresql with Django

---
+++ app/app/calc.py
@@ -0,0 +1,20 @@
+"""
+Calculation function
+"""
+def add(x, y):
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
+    return x + y
+
+def subtract(x, y):
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
+    return x - y
